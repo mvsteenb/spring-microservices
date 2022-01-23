5888567ae9e7897c3c045ee0fe127f01959a159f
mvsteenb <flexit8678>
2022-01-23 12:46:00 +0100

    restful webservices

---
+++ 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package be.flexlineitsolutions.udemy.microservices.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ 01-restful-web-services/src/test/java/be/flexlineitsolutions/udemy/microservices/rest/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package be.flexlineitsolutions.udemy.microservices.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
