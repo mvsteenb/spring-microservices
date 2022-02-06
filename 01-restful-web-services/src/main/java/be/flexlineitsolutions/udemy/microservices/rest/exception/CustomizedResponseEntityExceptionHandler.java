837d26ef745d64eaf4d4f05d76ac564de166c678
mvsteenb <flexit8678>
2022-02-06 12:41:17 +0100

    more webservices

---
+++ 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package be.flexlineitsolutions.udemy.microservices.rest.exception;
+
+import be.flexlineitsolutions.udemy.microservices.rest.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+
+@ControllerAdvice // shared across multiple controllers
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request) throws Exception {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(UserNotFoundException.class)
+  public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+    ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+  }
+}
--- 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/user/UserNotFoundException.java
+++ 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/user/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package be.flexlineitsolutions.udemy.microservices.rest.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
   public UserNotFoundException(String s) {
     super(s);
