ab4b54a21c44dc25cf87a47542d1f54876c60067
mvsteenb <flexit8678>
2022-02-12 13:41:39 +0100

    added i18n

--- 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/helloworld/HelloWorldController.java
+++ 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package be.flexlineitsolutions.udemy.microservices.rest.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+  @Autowired
+  private MessageSource messageSource;
 
   @GetMapping(path="/hello-world")
   public String helloWorld() {
@@ -20,5 +29,10 @@ public class HelloWorldController {
   @GetMapping(path="/hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
+  }
+
+  @GetMapping(path="/hello-world-i18n")
+  public String helloWorldInternationalized() {
+    return messageSource.getMessage("goodmorning.message", null, "Default message", LocaleContextHolder.getLocale());
   }
 }
