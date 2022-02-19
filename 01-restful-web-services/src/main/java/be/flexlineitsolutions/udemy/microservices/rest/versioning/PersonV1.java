636ae38e5653775749bcef06ce1485ec20adcd54
mvsteenb <flexit8678>
2022-02-19 17:10:15 +0100

    versioning

---
+++ 01-restful-web-services/src/main/java/be/flexlineitsolutions/udemy/microservices/rest/versioning/PersonV1.java
@@ -0,0 +1,21 @@
+package be.flexlineitsolutions.udemy.microservices.rest.versioning;
+
+public class PersonV1 {
+
+  private String name;
+
+  public PersonV1() {
+  }
+
+  public PersonV1(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
