c2dc1cfb323ef83c957cf43bab2d76b53acdba59
Mario Van Steenberghe <mario.vansteenberghe@gmail.com>
2022-02-24 19:26:45 +0100

    adding repository

--- 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CurrencyExchangeController.java
+++ 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,14 +1,19 @@
 package be.flexlineitsolutions.currencyexchangeservice;
 
 import be.flexlineitsolutions.currencyexchangeservice.model.ExchangeValue;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 @RestController
 public class CurrencyExchangeController {
+
+	private final Environment environment;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
@@ -16,6 +21,7 @@ public class CurrencyExchangeController {
 				.conversionMultiple(BigDecimal.valueOf(1000L))
 				.from(from)
 				.to(to)
+				.port(Integer.parseInt(environment.getProperty("local.server.port")))
 				.build();
 	}
 
--- 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/model/ExchangeValue.java
+++ 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/model/ExchangeValue.java
@@ -5,17 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class ExchangeValue {
 
+	@Id
+	@GeneratedValue
 	private Long id;
+
+	@Column(name = "currency_from")
 	private String from;
+
+	@Column(name = "currency_to")
 	private String to;
+
 	private BigDecimal conversionMultiple;
+	private int port;
 
 }
