c0c2faaf90a279728f7304bc1c9b603fbda19c7e
Mario Van Steenberghe <mario.vansteenberghe@gmail.com>
2022-02-25 16:48:14 +0100

    implemented RestTemplate

--- 03-microservices/currency-conversion-service/src/main/java/be/flexlineitsolutions/currencyconversionservice/model/CurrencyConversionBean.java
+++ 03-microservices/currency-conversion-service/src/main/java/be/flexlineitsolutions/currencyconversionservice/model/CurrencyConversionBean.java
@@ -1,2 +1,24 @@
-package be.flexlineitsolutions.currencyconversionservice;public class CurrencyConversionBean {
+package be.flexlineitsolutions.currencyconversionservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CurrencyConversionBean {
+
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionMultiple;
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmount;
+	private int port;
+
 }
