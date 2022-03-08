56bafc94ce0912980709382dd375d0ef3cfa25d9
mario.vansteenberghe <mario.vansteenberghe@liantis.be>
2022-03-08 12:26:17 +0100

    added naming server

---
+++ 03-microservices/naming-server/src/main/java/be/flexlineitsolutions/namingserver/NamingServerApplication.java
@@ -0,0 +1,13 @@
+package be.flexlineitsolutions.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
---
+++ 03-microservices/naming-server/src/test/java/be/flexlineitsolutions/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.flexlineitsolutions.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
