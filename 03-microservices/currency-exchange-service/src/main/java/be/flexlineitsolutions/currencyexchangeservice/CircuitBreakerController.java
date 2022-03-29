30ca1077dfcfcec4780e007fe46e216453b06d28
mario.vansteenberghe <mario.vansteenberghe@liantis.be>
2022-03-29 16:31:37 +0200

    circuit breaker

--- 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CircuitBreakerController.java
+++ 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,17 @@
 package be.flexlineitsolutions.currencyexchangeservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     public String sampleAPI() {
-        return "some sample API";
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
     }
 
 }
