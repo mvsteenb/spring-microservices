d335ac6731e93a4180e284363aa57dd39b712b93
Mario Van Steenberghe <mario.vansteenberghe@gmail.com>
2022-04-16 23:11:23 +0200

    circuitbreaker

--- 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CircuitBreakerController.java
+++ 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CircuitBreakerController.java
@@ -1,7 +1,8 @@
 package be.flexlineitsolutions.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,13 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     //@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
     @CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name="sample-api")  // e.g. in 10s => allow only 1000s calls to sample API
+    @Bulkhead(name="sample-api")
     public String sampleAPI() {
         log.info("Sample API call received !");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
+//        return "sample api";
     }
 
     public String hardcodedResponse(Exception e) {
