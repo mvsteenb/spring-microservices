13c3c428f60441a6646860ca7f43c422034440e8
mario.vansteenberghe <mario.vansteenberghe@liantis.be>
2022-04-16 22:33:41 +0200

    circuit breaker

--- 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CircuitBreakerController.java
+++ 03-microservices/currency-exchange-service/src/main/java/be/flexlineitsolutions/currencyexchangeservice/CircuitBreakerController.java
@@ -1,17 +1,28 @@
 package be.flexlineitsolutions.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Slf4j
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
+    //@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleAPI() {
+        log.info("Sample API call received !");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception e) {
+        return "fallback-response";
     }
 
 }
